6500389b10c8eeaf6566887e9d83a68c6e9a02eb
chrisfaux95 <chrisfaux95@gmail.com>
2020-09-10 17:30:38 -0400

    skeleton for api routes

---
+++ routes/api-routes.js
@@ -0,0 +1,25 @@
+
+var apiRoutes = app => {
+    // GET WORKOUTS
+    app.get("/api/workouts", (req, res) => {
+        
+    });
+
+    // PUT WORKOUTS
+    app.put("/api/workouts", (req, res) => {
+        
+    })
+
+    // POST WORKOUTS
+    app.post("/api/workouts", (req, res) => {
+        
+    });
+
+    // GET WORKOUTS RANGE
+    app.get("/api/workouts/range", (req, res) => {
+        
+    })
+
+}
+
+module.exports = apiRoutes;
