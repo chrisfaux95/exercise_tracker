c6a84579322ef7a5fd1f8bf12c36b8c632621869
chrisfaux95 <chrisfaux95@gmail.com>
2020-09-12 19:20:13 -0400

    fixed date thing

--- models/workoutModel.js
+++ models/workoutModel.js
@@ -5,7 +5,7 @@ const workoutSchema = new Schema(
     {
         day: {
             type: Date,
-            default: Date.now
+            default: () => new Date()
         },
         exercises: [
             {
