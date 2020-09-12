3296991fe550e4bec14b98c4c6809b320f1795ca
chrisfaux95 <chrisfaux95@gmail.com>
2020-09-12 18:13:12 -0400

    added post method

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,6 +1,14 @@
 const router = require("express").Router();
 const Exercise = require("../models/exerciseModel.js");
 
+router.post("/workouts", (req, res) => {
+    Exercise.create(req.body).then(dbExercise => {
+        res.json(dbExercise);
+    }).catch(err => {
+        res.status(400).json(err);
+    });
+});
+
 router.get("/workouts", (req, res) => {
     Exercise.find({}).sort({ date: -1 }).then(dbExercise => {
         res.json(dbExercise);
