eff636eadd91d2e9dac65628ceb7939f2dbc7b04
chrisfaux95 <chrisfaux95@gmail.com>
2020-09-12 19:12:21 -0400

    fixed error

--- models/workoutModel.js
+++ models/workoutModel.js
@@ -33,10 +33,11 @@ const workoutSchema = new Schema(
     { toJSON: { virtuals: true } }
 );
 
-workoutSchema.virtual('totalDuration').get(() => {
+workoutSchema.virtual('totalDuration').get(function () {
     return this.exercises.reduce(
-        (total, exercise) => total + exercise.duration
-    )
+        (total, exercise) => {
+            return total + exercise.duration;
+        }, 0);
 })
 
 const Workout = mongoose.model('Workout', workoutSchema);
